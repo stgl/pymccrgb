821804cf1414c3a85f54960902f1544ca59d03fe
rmsare <robertmsare@gmail.com>
2019-06-13 16:12:46 -0700

    Use alabaster theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,7 +110,11 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+import alabaster
+
+html_theme_path = [alabaster.get_path()]
+extensions = ['alabaster']
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
