51deff8870cbfa9ed12d1407634ab27921d6f0ff
rmsare <robertmsare@gmail.com>
2019-06-13 20:37:04 -0700

    Add placeholder IO functions

--- pymccrgb/io.py
+++ pymccrgb/io.py
@@ -1,1 +1,16 @@
 """ Convenience functions for loading point clouds in various formats """
+
+import numpy as np
+import pdal
+
+
+def load_txt(filename, usecols, userows=None):
+    pass
+
+
+def load_las(filename):
+    pass
+
+
+def load_obj(filename):
+    pass
