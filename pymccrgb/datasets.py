e18110b8a2e4a61a9b4f06b0e8f8332c0de7f43b
rmsare <robertmsare@gmail.com>
2019-06-13 20:35:40 -0700

    Add placeholders for I/O and datasets submodules

---
+++ pymccrgb/datasets.py
@@ -0,0 +1,13 @@
+""" Load example datasets of lidar and photogrammetric poitn clouds """
+
+
+def load_mammoth_lidar(n_points=None):
+    pass
+
+
+def load_mammoth_sfm(n_points=None):
+    pass
+
+
+def load_idaho_lidar(n_points=None):
+    pass
---
+++ pymccrgb/io.py
@@ -0,0 +1,1 @@
+""" Convenience functions for loading point clouds in various formats """
