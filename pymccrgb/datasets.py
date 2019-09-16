310adf28d7ce685bef626e256e307a8834c3cf99
rmsare <robertmsare@gmail.com>
2019-09-16 14:25:28 -0700

    Implement dataset loading function

--- pymccrgb/datasets.py
+++ pymccrgb/datasets.py
@@ -1,13 +1,23 @@
-""" Load example datasets of lidar and photogrammetric poitn clouds """
+""" Load example datasets of lidar and photogrammetric point clouds """
+
+import os
+
+from io import load_txt
+
+directories = os.path.dirname(__file__)
+path = "/".join(directories.split("/")[0:-1])
+DATA_DIRECTORY = os.path.join(path, "docs/source/examples/data")
 
 
-def load_mammoth_lidar(n_points=None):
-    pass
+def load_kaibab_lidar(npoints=None):
+    raise NotImplementedError("This method has not yet been implemented.")
 
 
-def load_mammoth_sfm(n_points=None):
-    pass
+def load_mammoth_lidar(npoints=None):
+    filename = os.path.join(DATA_DIRECTORY, "mammoth_lidar'txt")
+    data = load_txt(filename, nrows=npoints)
+    return data
 
 
-def load_idaho_lidar(n_points=None):
-    pass
+def load_mammoth_sfm(npoints=None):
+    raise NotImplementedError("This method has not yet been implemented.")
