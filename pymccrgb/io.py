9bba00724f921f0c73b848daeebb84d3cf037a19
rmsare <robertmsare@gmail.com>
2019-09-16 14:17:14 -0700

    Implement load_txt and remove old function

--- pymccrgb/io.py
+++ pymccrgb/io.py
@@ -4,8 +4,43 @@ import numpy as np
 import pdal
 
 
-def load_txt(filename, usecols, userows=None):
-    pass
+def load_txt(filename, usecols=range(6), userows=None, nrows=None):
+    """ Loads specified rows and columns from text file as numpy array
+
+    Args:
+        filename: Filename of text file containing point cloud
+
+        usecols: List of column indices to load
+            Default: First six columns, e.g.,  (x, y, z, r, g, b)
+
+        userows: List of rows to load. Overrides nrows argument
+            Default: All rows
+
+        nrows: Number of random rows to load. Ignored if userows is given.
+            Default: Not used.
+
+    Returns: A data array of shape (nrows x ncols)
+    """
+
+    if userows is None:
+        with open(filename, "r") as f:
+            for i, s in enumerate(f):
+                pass
+        nlines = i + 1
+        if nrows is not None:
+            userows = np.random.choice(nlines, size=nrows)
+        else:
+            userows = range(nlines)
+
+    data = []
+    with open(filename, "r") as f:
+        for i, s in enumerate(f):
+            if i in userows:
+                row = s.split(",")
+                row = np.asarray(row)
+                row = row[usecols]
+                data.append(row)
+    return np.array(data)
 
 
 def load_las(filename):
