1452c3e6ad5416509196151ea126d5256ad81a43
rmsare <robertmsare@gmail.com>
2019-09-17 14:09:03 -0700

    Add docstrings

--- pymccrgb/datasets.py
+++ pymccrgb/datasets.py
@@ -10,18 +10,42 @@ DATA_DIRECTORY = os.path.join(path, "docs/source/examples/data")
 
 
 def load_kaibab_lidar(npoints=None):
+    """Loads sample dataset from Kaibab Plateau, Arizona, USA lidar dataset
+
+    Args:
+        npoints: Optional number of (random) points to load from file
+
+    Returns:
+        A data ndarray
+    """
     filename = os.path.join(DATA_DIRECTORY, "kaibab_lidar.laz")
     data = load_las(filename, nrows=npoints)
     return data
 
 
 def load_mammoth_lidar(npoints=None):
+    """Loads sample dataset from Mammoth Mountain, California, USA lidar dataset
+
+    Args:
+        npoints: Optional number of (random) points to load from file
+
+    Returns:
+        A data ndarray
+    """
     filename = os.path.join(DATA_DIRECTORY, "mammoth_lidar.laz")
     data = load_las(filename, nrows=npoints)
     return data
 
 
 def load_mammoth_sfm(npoints=None):
+    """Loads sample dataset from Mammoth Mountain, California, USA structure-from-motion dataset
+
+    Args:
+        npoints: Optional number of (random) points to load from file
+
+    Returns:
+        A data ndarray
+    """
     filename = os.path.join(DATA_DIRECTORY, "mammoth_sfm.laz")
     data = load_las(filename, nrows=npoints)
     return data
