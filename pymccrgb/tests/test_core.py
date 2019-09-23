167a4137578c676ee86038678984728c440e0bc4
rmsare <robertmsare@gmail.com>
2019-09-23 10:03:17 -0700

    Add tests for core algorithms and feature calculation

---
+++ pymccrgb/tests/test_core.py
@@ -0,0 +1,66 @@
+""" Test Python MCC bindings and MCC-RGB algorithm """
+
+import os
+
+import pytest
+import unittest
+
+import numpy as np
+
+from context import pymccrgb
+
+TEST_DIR = os.path.dirname(__file__)
+TEST_SCALES = [0.5, 1, 1.5]
+TEST_TOLS = [0.01, 0.05, 0.3, 0.5, 1]
+
+
+class MCCTestCase(unittest.TestCase):
+    def setUp(self):
+        self.data = pymccrgb.io.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
+
+    def _test_classify_ground_mcc(self, scale, tol):
+        test = pymccrgb.core.classify_ground(self.data, scale, tol)
+        true = np.load(os.path.join(TEST_DIR, f"classification_mcc_{scale}_{tol}.npy"))
+        self.assertEqual(
+            test,
+            true,
+            f"MCC ground classification is incorrect for scale {scale} and height tolerance {tol}",
+        )
+
+    def test_mcc_classification(self):
+        for scale in TEST_SCALES:
+            for tol in TEST_TOLS:
+                self._test_classify_ground_mcc(scale, tol)
+
+    def test_mcc_default(self):
+        test_points, test_labels = pymccrgb.core.mcc(self.data, verbose=True)
+        true_points, true_labels = np.load(
+            os.path.join(TEST_DIR, f"ground_labels_mcc_default.npy")
+        )
+        self.assertTrue(
+            np.allclose(test_points, true_points),
+            "Ground points are incorrect for default MCC configuration",
+        )
+        self.assertTrue(
+            np.allclose(test_labels, true_labels),
+            "Classification is incorrect for default MCC configuration",
+        )
+
+
+class MCCRGBTestCase(unittest.TestCase):
+    def setUp(self):
+        self.data = pymccrgb.io.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
+
+    def test_mcc_rgb_default(self):
+        test_points, test_labels = pymccrgb.core.mcc_rgb(self.data, verbose=True)
+        true_points, true_labels, true_updated = np.load(
+            os.path.join(TEST_DIR, f"ground_labels_mc_crgb_default.npy")
+        )
+        self.assertTrue(
+            np.allclose(test_points, true_points),
+            "Ground points are incorrect for default MCC-RGB configuration",
+        )
+        self.assertTrue(
+            np.allclose(test_labels, true_labels),
+            "Classification is incorrect for default MCC-RGB configuration",
+        )
---
+++ pymccrgb/tests/test_features.py
@@ -0,0 +1,44 @@
+""" Test computing color features from point cloud """
+
+import os
+
+import pytest
+import unittest
+
+import numpy as np
+
+from context import pymccrgb
+
+TEST_DIR = os.path.dirname(__file__)
+
+# Indices of derived features in test data
+L_INDEX = 0
+A_INDEX = 1
+B_INDEX = 2
+NGRDVI_INDEX = 3
+VDVI_INDEX = 4
+DEFAULT_FEATURE_INDEXES = [A_INDEX, B_INDEX, NGRDVI_INDEX]
+
+
+class ColorFeatureTestCase(unittest.TestCase):
+    def setUp(self):
+        self.data = pymccrgb.io.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
+        self.target = np.load(os.path.join(TEST_DIR, "features_rgb.npy"))
+
+    def test_calculate_color_features(self):
+        test = pymccrgb.features.calculate_color_features(self.data)
+        true = self.target[:, DEFAULT_FEATURE_INDEXES]
+        self.assertTrue(
+            np.allclose(test, true),
+            "Default color feature calculation (a, b, NGRDVI) is incorrect",
+        )
+
+    def test_calculate_ngrdvi(self):
+        test = pymccrgb.features.calculate_ngrdvi(self.data)
+        true = self.target[:, NGRDVI_INDEX]
+        self.assertTrue(np.allclose(test, true), "NGRDVI calculation is incorrect")
+
+    def test_calculate_vdvi(self):
+        test = pymccrgb.features.calculate_vdvi(self.data)
+        true = self.target[:, VDVI_INDEX]
+        self.assertTrue(np.allclose(test, true), "VDVI calculation is incorrect")
