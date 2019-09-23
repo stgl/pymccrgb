add01347055e0216ba1e521e906da68f8a3577db
rmsare <robertmsare@gmail.com>
2019-09-23 10:55:31 -0700

    Use full module path

--- pymccrgb/tests/test_core.py
+++ pymccrgb/tests/test_core.py
@@ -9,8 +9,6 @@ import numpy as np
 
 from context import pymccrgb
 
-from pymccrgb import core, ioutils
-
 TEST_DIR = os.path.dirname(__file__)
 TEST_SCALES = [0.5, 1, 1.5]
 TEST_TOLS = [0.01, 0.05, 0.3, 0.5, 1]
@@ -18,10 +16,10 @@ TEST_TOLS = [0.01, 0.05, 0.3, 0.5, 1]
 
 class MCCTestCase(unittest.TestCase):
     def setUp(self):
-        self.data = ioutils.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
+        self.data = pymccrgb.ioutils.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
 
     def _test_classify_ground_mcc(self, scale, tol):
-        test = core.classify_ground(self.data, scale, tol)
+        test = pymccrgb.core.classify_ground(self.data, scale, tol)
         true = np.load(os.path.join(TEST_DIR, f"classification_mcc_{scale}_{tol}.npy"))
         self.assertEqual(
             test,
@@ -35,7 +33,7 @@ class MCCTestCase(unittest.TestCase):
                 self._test_classify_ground_mcc(scale, tol)
 
     def test_mcc_default(self):
-        test_points, test_labels = core.mcc(self.data, verbose=True)
+        test_points, test_labels = pymccrgb.core.mcc(self.data, verbose=True)
         true_points, true_labels = np.load(
             os.path.join(TEST_DIR, f"ground_labels_mcc_default.npy")
         )
@@ -51,10 +49,10 @@ class MCCTestCase(unittest.TestCase):
 
 class MCCRGBTestCase(unittest.TestCase):
     def setUp(self):
-        self.data = ioutils.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
+        self.data = pymccrgb.ioutils.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
 
     def test_mcc_rgb_default(self):
-        test_points, test_labels = core.mcc_rgb(self.data, verbose=True)
+        test_points, test_labels = pymccrgb.core.mcc_rgb(self.data, verbose=True)
         true_points, true_labels, true_updated = np.load(
             os.path.join(TEST_DIR, f"ground_labels_mc_crgb_default.npy")
         )
--- pymccrgb/tests/test_features.py
+++ pymccrgb/tests/test_features.py
@@ -9,8 +9,6 @@ import numpy as np
 
 from context import pymccrgb
 
-from pymccrgb import features, ioutils
-
 TEST_DIR = os.path.dirname(__file__)
 
 # Indices of derived features in test data
@@ -24,11 +22,11 @@ DEFAULT_FEATURE_INDEXES = [A_INDEX, B_INDEX, NGRDVI_INDEX]
 
 class ColorFeatureTestCase(unittest.TestCase):
     def setUp(self):
-        self.data = ioutils.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
+        self.data = pymccrgb.ioutils.load_las(os.path.join(TEST_DIR, "points_rgb.laz"))
         self.target = np.load(os.path.join(TEST_DIR, "features_rgb.npy"))
 
     def test_calculate_color_features(self):
-        test = features.calculate_color_features(self.data)
+        test = pymccrgb.features.calculate_color_features(self.data)
         true = self.target[:, DEFAULT_FEATURE_INDEXES]
         self.assertTrue(
             np.allclose(test, true),
@@ -36,11 +34,11 @@ class ColorFeatureTestCase(unittest.TestCase):
         )
 
     def test_calculate_ngrdvi(self):
-        test = features.calculate_ngrdvi(self.data)
+        test = pymccrgb.features.calculate_ngrdvi(self.data)
         true = self.target[:, NGRDVI_INDEX]
         self.assertTrue(np.allclose(test, true), "NGRDVI calculation is incorrect")
 
     def test_calculate_vdvi(self):
-        test = features.calculate_vdvi(self.data)
+        test = pymccrgb.features.calculate_vdvi(self.data)
         true = self.target[:, VDVI_INDEX]
         self.assertTrue(np.allclose(test, true), "VDVI calculation is incorrect")
